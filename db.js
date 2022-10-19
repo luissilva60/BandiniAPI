9ae4e27d896e812a8af1726288b4168de503892d
Luís Silva <ifaf.silva12@gmail.com>
2022-10-19 14:06:13 +0100

    c

--- db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Option 1: Passing a connection URI
-
-const sequelize = new Sequelize('postgres://postgres:XJTQ76Aj7aN6BLX@bandini-db.internal:5432/bandini-db') // Example for postgres
-
-
-
-module.exports =  sequelize;
